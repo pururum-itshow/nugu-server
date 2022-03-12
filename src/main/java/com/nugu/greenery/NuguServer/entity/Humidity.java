d0e6ed5a6d7270b3a291e71954ff2e6b38b758e0
SRin23 <serin4723@naver.com>
2022-03-13 00:29:13 +0900

    (#1) Entity 구현

--- src/main/java/com/nugu/greenery/NuguServer/entity/Humidity.java
+++ src/main/java/com/nugu/greenery/NuguServer/entity/Humidity.java
@@ -1,5 +1,26 @@
 package com.nugu.greenery.NuguServer.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+//entity
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Humidity {
-    //습도
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
+    private long id;
+
+    @NonNull
+    @Column(name = "datetime_value", nullable = false)
+    private Timestamp datetime;
+
+    @Column(name = "humidity_value", nullable = false)
+    private String humidity;
 }
