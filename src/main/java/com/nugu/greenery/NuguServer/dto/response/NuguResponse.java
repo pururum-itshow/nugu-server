2c2167e3591f5036750e5a8cf90a3efacd09cf68
SRin23 <serin4723@naver.com>
2022-03-21 02:04:39 +0900

    (add) DTO 초기 구현 - #2

---
+++ src/main/java/com/nugu/greenery/NuguServer/dto/response/NuguResponse.java
@@ -0,0 +1,10 @@
+package com.nugu.greenery.NuguServer.dto.response;
+
+import java.util.Map;
+
+public class NuguResponse {
+    private String version;
+    private String resultCode;
+    private Map<String, Object> output;
+    //private Directives directives;
+}
